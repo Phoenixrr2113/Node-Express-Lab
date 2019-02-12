42441e17f3683501c5e47994c228bd43862ddd45
Randy Wilson <phoenixrr2113@gmail.com>
2019-02-12 16:24:33 -0500

    finished routes. will refactor later with mvp goals

--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -4,22 +4,22 @@ const Blogs = require('../../data/db');
 
 const router = express.Router();
 
+router.get('/:id', (req, res) => {
+	Blogs.findById(req.params.id)
+		.then(blog => {
+			if (blog) {
+				res.status(200).json(blog);
+			} else {
+				res.status(404).json('blog not found');
+			}
+		})
+		.catch(err => res.status(500).json(err));
+});
+
 router.get('/', async (req, res) => {
 	try {
 		const blogs = await Blogs.find(req.body);
 		res.status(200).json(blogs);
-	} catch (error) {
-		res.status(500).json({
-			message: 'Error creating the posts',
-		});
-	}
-});
-
-router.get('/:id', async (req, res) => {
-	console.log(req);
-	try {
-		const blog = await Blogs.findById(req.params);
-		res.status(200).json(blog);
 	} catch (error) {
 		res.status(500).json({
 			message: 'Error creating the posts',
@@ -39,15 +39,27 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// router.post('/', async (req, res) => {
-// 	try {
-// 		const blog = await Blogs.remove(req.params);
-// 		res.status(200).json(blog);
-// 	} catch (error) {
-// 		res.status(500).json({
-// 			message:
-// 				'error: "There was an error while deleting the post to the database',
-// 		});
-// 	}
-// });
+router.post('/:id', async (req, res) => {
+	try {
+		const blog = await Blogs.remove(req.params.id);
+		res.status(200).json(blog);
+	} catch (error) {
+		res.status(500).json({
+			message:
+				'error: "There was an error while deleting the post to the database',
+		});
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		const blog = await Blogs.update(req.params.id, req.body);
+		res.status(200).json(blog);
+	} catch (error) {
+		res.status(500).json({
+			message:
+				'error: "There was an error while deleting the post to the database',
+		});
+	}
+});
 module.exports = router;
